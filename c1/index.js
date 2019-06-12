ee4aa978a6d3f4064b3b0f95e089a404951136d9
bubigithub <bubigithub@github.com>
2019-06-12 22:06:20 +0200

    cas1

---
+++ c1/index.js
@@ -0,0 +1,21 @@
+console.log('Helloz!');
+ime = "Petar";
+var prezime = "Grozdanov";
+let vozrast = 17;
+console.log(ime);
+
+
+console.log(`sdijsidjsijd ${prezime} diskdijsd`);
+
+function test(){
+    // console.log(ime);
+    // console.log(prezime);
+    // console.log(vozrast);
+
+     a = 10;
+    var b = 20;
+    let c = 30;
+}
+console.log (a);
+console.log (b);
+console.log (c);
