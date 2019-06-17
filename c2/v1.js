ff98c204a9106669c77f9bfb7228c38b777bd00e
bubigithub <bubigithub@github.com>
2019-06-17 22:05:12 +0200

    chas 2 v2.0 eksport

--- c2/v1.js
+++ c2/v1.js
@@ -1,17 +1,5 @@
-// sekoj tret paren broj
-function everyThird(){
-    var i = 0;
-    var b = 0;
-    while(i < 100){
-        i++;
-        if(i % 2 == 0){
-            b++;
-        }
-        if(i % 2 == 0 && b == 3){
-            console.log(i);
-            b = 0;
-        }
-    }
-}
-everyThird();
+var func = require('./v2');
+func.everyThird();
 
+var r = func.arrSum([248, 21, 95, 239, 46]);
+console.log(r);
--- c2/v2.js
+++ c2/v2.js
@@ -1,4 +1,4 @@
-//SUMA NA BROEVI VO NIZA
+
 function arrSum(a){
     var c = 0;
     for(var i = 0; i < a.length; i++){
@@ -13,5 +13,23 @@ function arrSum(a){
         return a.reduce ((a, b) => a + b);
     }
 }
-var r = arrSum([248, 21, 95, 239, 46]);
-console.log(r);
+//SUMA NA BROEVI VO NIZA
+// sekoj tret paren broj
+function everyThird(){
+    var i = 0;
+    var b = 0;
+    while(i < 100){
+        i++;
+        if(i % 2 == 0){
+            b++;
+        }
+        if(i % 2 == 0 && b == 3){
+            console.log(i);
+            b = 0;
+        }
+    }
+}
+module.exports = {
+    arrSum,
+    everyThird
+};
